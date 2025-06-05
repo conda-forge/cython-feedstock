5da6efb3f64812182de839da94073034d15ce4b5
H. Vetinari <h.vetinari@gmx.com>
2025-06-06 07:59:13 +1100

    Revert "Update run_test.py"
    This reverts commit 17b51daa05e37edfe3c3c242714ee8c96b7f9171.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -35,7 +35,6 @@ print('sys.version: %r' % sys.version)
 print('PATH: %r' % os.environ['PATH'])
 print('CWD: %r' % os.getcwd())
 
-import setuptools
 from distutils.spawn import find_executable
 from distutils.core import setup
 from distutils.extension import Extension
